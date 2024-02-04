67e69f4088beb5afae89bd0c7c036c03bbf7fda8
Przemyslaw Dabrowski <przemyslaw.dabrowski@aptiv.com>
2024-02-04 13:43:11 +0100

    Settings reorganized

--- src/simulation/init/json.rs
+++ src/simulation/init/json.rs
@@ -4,48 +4,53 @@ use json::{self, JsonValue};
 
 use crate::{CollisionChecker, LanderRunner, LanderState, Physics, Terrain};
 
+macro_rules! get_json {
+    ($json:ident,$($key:literal),+, $func:ident) => {
+            get_json!($json$([$key])+, concat!($("/",$key),+), $func)
+    };
+
+    ($value:expr, $key:expr, $func:ident) => {
+        $value
+            .$func()
+            .ok_or(concat!("Couldn't find ", $key))?
+    };
+}
+
+macro_rules! json_value_or_err {
+    ($json:ident,$($key:literal),+) => {
+            {
+                let value = &$json$([$key])+;
+                if value.is_null() {
+                    Err(concat!("Lacking", concat!($("/",$key),+), " key"))
+                }else{
+                    Ok(value)
+                }
+            }
+    };
+}
+
 pub fn from_file(file_path: String) -> Result<LanderRunner, String> {
-    runner_from_json(read_json(file_path)?)
+    let json = read_json(file_path)?;
+    Ok(LanderRunner::new(
+        parse_lander(&json)?,
+        get_json!(json, "NumOfRunners", as_usize),
+        Physics::default(),
+        CollisionChecker::default(),
+        parse_terrain(json_value_or_err!(json, "Terrain")?)?,
+    ))
 }
 
 fn read_json(file_path: String) -> Result<JsonValue, String> {
     let mut file_content = String::new();
-    let mut file = File::open(file_path).map_err(|_| "Path does not exist")?;
+    let mut file = File::open(file_path).map_err(|e| format!("Error while opening file: {e}"))?;
 
     file.read_to_string(&mut file_content)
-        .map_err(|_| "Failed to read file")?;
-    json::parse(&file_content).map_err(|e| e.to_string())
+        .map_err(|e| format!("Failed to read file: {e}"))?;
+    json::parse(&file_content).map_err(|e| format!("Json error: {e}"))
 }
 
-fn runner_from_json(json: JsonValue) -> Result<LanderRunner, String> {
-    let mut initial_lander_state = LanderState::default();
-
-    macro_rules! get_json {
-        ($($key:literal),+, $func:ident) => {
-                get_json!(json$([$key])+, concat!($("/",$key),+), $func)
-        };
-
-        ($value:expr, $key:expr, $func:ident) => {
-            $value
-                .$func()
-                .ok_or(concat!("Couldn't find ", $key))?
-        };
-    }
-
-    initial_lander_state.x = get_json!("Lander", "X", as_f64);
-    initial_lander_state.y = get_json!("Lander", "Y", as_f64);
-    initial_lander_state.vx = get_json!("Lander", "HSpeed", as_f64);
-    initial_lander_state.vy = get_json!("Lander", "VSpeed", as_f64);
-    initial_lander_state.fuel = get_json!("Lander", "Fuel", as_i32);
-    initial_lander_state.angle = get_json!("Lander", "Angle", as_f64);
-    initial_lander_state.power = get_json!("Lander", "Power", as_i32);
-
-    let terrain_array = &json["Terrain"];
-    if terrain_array.is_null() {
-        return Err("Lacking Terrain key".to_owned());
-    }
-
-    let terrain = terrain_array
+fn parse_terrain(terrain_array: &JsonValue) -> Result<Terrain, String> {
+    terrain_array
         .members()
         .map(|point_json| {
             let err_str = "Terrain has to contain numeric landpoints";
@@ -62,13 +67,18 @@ fn runner_from_json(json: JsonValue) -> Result<LanderRunner, String> {
                 Ok::<(Vec<f64>, Vec<f64>), String>((xs, ys))
             },
         )
-        .map(|(x, y)| Terrain { x, y })?;
+        .map(|(x, y)| Terrain { x, y })
+}
 
-    Ok(LanderRunner::new(
-        initial_lander_state,
-        get_json!("NumOfRunners", as_usize),
-        Physics::default(),
-        CollisionChecker::default(),
-        terrain,
-    ))
+fn parse_lander(json: &JsonValue) -> Result<LanderState, String> {
+    let mut initial_lander_state = LanderState::default();
+
+    initial_lander_state.x = get_json!(json, "Lander", "X", as_f64);
+    initial_lander_state.y = get_json!(json, "Lander", "Y", as_f64);
+    initial_lander_state.vx = get_json!(json, "Lander", "HSpeed", as_f64);
+    initial_lander_state.vy = get_json!(json, "Lander", "VSpeed", as_f64);
+    initial_lander_state.fuel = get_json!(json, "Lander", "Fuel", as_i32);
+    initial_lander_state.angle = get_json!(json, "Lander", "Angle", as_f64);
+    initial_lander_state.power = get_json!(json, "Lander", "Power", as_i32);
+    Ok(initial_lander_state)
 }

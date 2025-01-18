8accb7ed7d88202cace671ed8f37c260ca4302fa
github-action-benchmark <github@users.noreply.github.com>
2025-01-18 13:21:56 +0000

    add App signle iteration benchmark (cargo) benchmark result for 2ccc6249ecfb49ff4c3d0b2c3403d143a9db1d4b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737205074223,
+  "lastUpdate": 1737206516454,
   "repoUrl": "https://github.com/Nizerlak/mars-lander-exercise",
   "entries": {
     "App signle iteration benchmark": [
@@ -71,6 +71,78 @@ window.BENCHMARK_DATA = {
             "name": "run_next_population_complicated_sim_hard_settings",
             "value": 8557301,
             "range": "± 299432",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "prdabr@gmail.com",
+            "name": "nizerlak",
+            "username": "Nizerlak"
+          },
+          "committer": {
+            "email": "prdabr@gmail.com",
+            "name": "nizerlak",
+            "username": "Nizerlak"
+          },
+          "distinct": true,
+          "id": "2ccc6249ecfb49ff4c3d0b2c3403d143a9db1d4b",
+          "message": "Revert \"Compare with previous results\"\n\nThis reverts commit b82bfda0f51bc616326cd66c9c98e1e80f1de39b.",
+          "timestamp": "2025-01-18T14:19:16+01:00",
+          "tree_id": "ed22067312182fed3fa05978b3edb8d1b8e4036e",
+          "url": "https://github.com/Nizerlak/mars-lander-exercise/commit/2ccc6249ecfb49ff4c3d0b2c3403d143a9db1d4b"
+        },
+        "date": 1737206516184,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "run_simple_sim_light_settings",
+            "value": 283025,
+            "range": "± 4180",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_complicated_sim_light_settings",
+            "value": 398929,
+            "range": "± 1456",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_simple_sim_hard_settings",
+            "value": 2126371,
+            "range": "± 37938",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_complicated_sim_hard_settings",
+            "value": 2703891,
+            "range": "± 9617",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_next_population_simple_sim_light_settings",
+            "value": 617376,
+            "range": "± 35405",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_next_population_complicated_sim_light_settings",
+            "value": 1005229,
+            "range": "± 53491",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_next_population_simple_sim_hard_settings",
+            "value": 4908984,
+            "range": "± 103424",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "run_next_population_complicated_sim_hard_settings",
+            "value": 8572509,
+            "range": "± 191128",
             "unit": "ns/iter"
           }
         ]
